ed2349bd7ddf7f8127617f1e2f6209a59bc45789
Christian Le <christianle@berkeley.edu>
2015-01-04 12:51:02 -0800

    add functionality to set favorite/default color and color will be set based on dominant color of icon (ignore grayscale colors i.e. white to gray to black)

--- app/src/main/java/com/cle/ambientnotifications/ControlActivity.java
+++ app/src/main/java/com/cle/ambientnotifications/ControlActivity.java
@@ -9,15 +9,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import android.widget.SeekBar;
@@ -42,27 +51,9 @@ public class ControlActivity extends Activity {
     private BluetoothGattCharacteristic characteristicTx;
     private BluetoothGattService gattService;
     private NotificationReceiver notifReceiver;
-    private String defaultColor = "150,150,0";
-    private String facebookColor = "59,89,152";
-    private String snapchatColor = "255,252,0";
-    private String messagingColor = "149,184,42";
-    private String phoneColor = "6,77,176";
 
-    private static final HashMap<String, String> notificationColors;
-    static {
-        notificationColors = new HashMap<>();
-        notificationColors.put("com.motorola.vzw.settings.extensions", "102,51,153");
-        notificationColors.put("com.facebook.orca", "59,89,152");
-        notificationColors.put("com.facebook.katana", "109,132,180");
-        notificationColors.put("com.snapchat.android", "255,252,0");
-        notificationColors.put("com.google.android.apps.inbox", "66,133,244");
-        notificationColors.put("com.google.android.gm", "219,68,55");
-        notificationColors.put("com.mailboxapp", "81,185,219");
-        notificationColors.put("com.google.android.talk", "27,162,97");
-        notificationColors.put("com.android.providers.telephony", "6,77,176");
-        notificationColors.put("com.android.vending", "0,0,0");
-        notificationColors.put("com.android.providers.downloads", "0,0,0");
-    }
+    private SharedPreferences notificationColors;
+    private String defaultColor;
 
     private String lastNotification = "";
 
@@ -123,7 +114,7 @@ public class ControlActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.gatt_services_characteristics);
+        setContentView(R.layout.color_selector);
 
         final Intent intent = getIntent();
 
@@ -154,6 +145,22 @@ public class ControlActivity extends Activity {
 
         Intent gattServiceIntent = new Intent(this, BLEService.class);
         bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+
+        notificationColors = getSharedPreferences(getApplicationContext().getPackageName(), 0);
+        boolean firstStart = notificationColors.getBoolean("firstStart", true);
+        defaultColor = notificationColors.getString("defaultColor", "150,150,0");
+
+        if (firstStart) {
+            SharedPreferences.Editor editor = notificationColors.edit();
+            editor.putBoolean("firstStart", false)
+                    .putString("defaultColor", "150,150,0")
+                    .putString("com.motorola.vzw.settings.extensions", "0,0,0")
+                    .putString("com.google.android.apps.inbox", "66,133,244")
+                    .putString("com.google.android.gm", "219,68,55")
+                    .putString("com.android.vending", "0,0,0")
+                    .putString("com.android.providers.downloads", "0,0,0");
+            editor.commit();
+        }
     }
 
     @Override
@@ -237,7 +244,7 @@ public class ControlActivity extends Activity {
     }
 
     private void readSeek(SeekBar seekBar,final int pos) {
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
@@ -245,13 +252,29 @@ public class ControlActivity extends Activity {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 makeChange();
             }
         });
+    }
+
+    public void onSetFavorite(View v) {
+        int[] colors = new int[] {
+                RGBFrame[0],
+                RGBFrame[1],
+                RGBFrame[2]
+        };
+
+        String strColor = Arrays.toString(colors);
+        String color = strColor.substring(1, strColor.length() - 1).replace(" ", "");
+
+        SharedPreferences.Editor editor = notificationColors.edit();
+        editor.putString("defaultColor", color);
+        editor.apply();
     }
 
     private void makeChange() {
@@ -264,9 +287,9 @@ public class ControlActivity extends Activity {
         }
 
         int sync = 0xa5;
-        int red = 255 - RGBFrame[0];
-        int green = 255 - RGBFrame[1];
-        int blue = 255 - RGBFrame[2];
+        int red = RGBFrame[0];
+        int green = RGBFrame[1];
+        int blue = RGBFrame[2];
         int checksum = red ^ green ^ blue;
 
         final byte[] sendTx = new byte[] {
@@ -297,26 +320,20 @@ public class ControlActivity extends Activity {
             }
         }
 
-        String color = notificationColors.get(pckt);
-        if (color == null) {
-            if (pckt.toLowerCase().contains("facebook")) {
-                color = facebookColor;
-            } else if (pckt.toLowerCase().contains("snapchat")) {
-                color = snapchatColor;
-            } else if (pckt.toLowerCase().contains("phon")) {
-                color = phoneColor;
-            } else if (pckt.toLowerCase().contains("messag")) {
-                color = messagingColor;
-            } else {
-                color = defaultColor;
-            }
+        String color;
+
+        if (notificationColors.contains(pckt)) {
+            color = notificationColors.getString(pckt, "100,100,100");
+        } else {
+            color = notificationColors.getString(pckt, getApplicationColor(pckt));
         }
+
         String[] colors = color.split(",");
 
         int sync = 0xa5;
-        int red = 255 - Integer.parseInt(colors[0]);
-        int green = 255 - Integer.parseInt(colors[1]);
-        int blue = 255 - Integer.parseInt(colors[2]);
+        int red = Integer.parseInt(colors[0]);
+        int green = Integer.parseInt(colors[1]);
+        int blue = Integer.parseInt(colors[2]);
         int checksum = red ^ green ^ blue;
 
         final byte[] sendTx = new byte[] {
@@ -348,9 +365,9 @@ public class ControlActivity extends Activity {
         }
 
         int sync = 0xa5;
-        int red = 0xff;
-        int green = 0xdd;
-        int blue = 0xff;
+        int red = 0x00;
+        int green = 0x33;
+        int blue = 0x00;
         int checksum = red ^ green ^ blue;
 
         final byte[] sendTx = new byte[] {
@@ -382,7 +399,7 @@ public class ControlActivity extends Activity {
         }
 
         int sync = 0xa5;
-        int off = 0xFF;
+        int off = 0x00;
 
         final byte[] sendTx = new byte[] {
                 (byte) sync,
@@ -398,11 +415,110 @@ public class ControlActivity extends Activity {
         }
     }
 
+    public String getApplicationColor(String pckt) {
+        String color;
+
+        try {
+            Drawable icon = getPackageManager().getApplicationIcon(pckt);
+            Bitmap bIcon = drawableToBitmap(icon);
+            int iWidth = bIcon.getWidth();
+            int iHeight = bIcon.getHeight();
+
+            int[] pixels = new int[iWidth * iHeight];
+            bIcon.getPixels(pixels, 0, iWidth, 0, 0, iWidth, iHeight);
+
+            int[] colors = getDominantColor(pixels);
+
+            String strColor = Arrays.toString(colors);
+            color = strColor.substring(1, strColor.length() - 1).replace(" ", "");
+
+            if (!notificationColors.contains(pckt)) {
+                SharedPreferences.Editor editor = notificationColors.edit();
+                editor.putString(pckt, color);
+                editor.apply();
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            color = defaultColor;
+        }
+
+        return color;
+    }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable)drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        int bWidth = bitmap.getWidth();
+        int bHeight = bitmap.getHeight();
+
+        if (bWidth * bHeight > 10000) {
+            bitmap = Bitmap.createScaledBitmap(bitmap, 100, (100 * bHeight) / bWidth, false);
+        }
+
+        return bitmap;
+    }
+
+    public int[] getDominantColor(int[] pixels) {
+        HashMap<Integer, Integer> colorMap = new HashMap();
+
+        for (int pixel : pixels) {
+            if (!isGrayScalePixel(pixel)) {
+                Integer counter = colorMap.get(pixel);
+
+                if (counter == null) {
+                    counter = 1;
+                } else {
+                    counter += 1;
+                }
+
+                colorMap.put(pixel, counter);
+            }
+        }
+
+        boolean init = true;
+        int maxColor = 0;
+
+        for (int color : colorMap.keySet()) {
+            if (init) {
+                maxColor = color;
+                init = false;
+            } else if (colorMap.get(color) > colorMap.get(maxColor)) {
+                maxColor = color;
+            }
+        }
+
+        int[] colorToUse = {
+                Color.red(maxColor),
+                Color.green(maxColor),
+                Color.blue(maxColor)
+        };
+
+        return colorToUse;
+    }
+
+    boolean isGrayScalePixel(int pixel){
+        int red = Color.red(pixel);
+        int green = Color.green(pixel);
+        int blue = Color.blue(pixel);
+
+        if (red == green && green == blue) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     class NotificationReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.d(TAG, "gets here");
             String event = intent.getStringExtra("notification_event");
             String pckt = intent.getStringExtra("notification_package");
 
